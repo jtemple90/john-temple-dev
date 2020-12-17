9bbcc3359851ea151fe816839cc3f7df0ebfaf8f
John Temple <johntemple@Alenas-iMac.local>
2020-12-17 11:38:32 -0700

    Updated css

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,16 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar-menu');
+const nav = document.querySelector('.nav');
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+nav.addEventListener('click', function() {
+    nav.classList.toggle('active');
+});
+
 
 // window.addEventListener('DOMContentLoaded', function() {
 //     const form = document.getElementById('user-form');
